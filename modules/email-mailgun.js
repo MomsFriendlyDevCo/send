2bf2de759698b5b4752af5278e93aef0fad384e3
Matt Carter <m@ttcarter.com>
2024-08-28 13:09:05 +1000

    feat: Email modules

---
+++ modules/email-mailgun.js
@@ -0,0 +1,34 @@
+import joyful from '@momsfriendlydevco/joyful';
+import nodemailer from 'nodemailer';
+import nodemailerMailgun from 'nodemailer-mailgun-transport';
+import SendEmail from '#modules/email';
+
+/**
+* Email module that uses Mailgun as the transport
+*/
+export default class SendEmailMailgun extends SendEmail {
+	name = 'Email-Mailgun';
+
+	validate(options) {
+		super.validate(options);
+
+		return joyful(options, joi => joi.object({
+			apiKey: joi.string().required(),
+			domain: joi.string().required(),
+		})
+			.unknown(true) // Allow downstream keys for mail email module
+		);
+	}
+
+	send(message, options) {
+		return super.send(message, {
+			...options,
+			nodemailer: nodemailer.createTransport(nodemailerMailgun({
+				auth: {
+					api_key: options.apiKey,
+					domain: options.domain,
+				},
+			})),
+		});
+	}
+}
---
+++ modules/email.js
@@ -0,0 +1,37 @@
+import joyful from '@momsfriendlydevco/joyful';
+import SendModule from '#lib/module';
+
+/**
+* Generic email sending module
+*
+* This module provides a basic module and should be extended by others
+*/
+export default class SendEmail extends SendModule {
+	name = 'Email';
+
+	validate(options) {
+		return joyful(options, joi => joi.object({
+			to: joi.string().required(),
+			from: joi.string().required(),
+			subject: joi.string().required(),
+			cc: joi.string().optional(),
+			bcc: joi.string().optional(),
+		})
+			.unknown(true) // Allow additional keys from other email-* modules
+		);
+	}
+
+	send(message, options) {
+		if (!options.transport) throw new Error('Emailer module requires a transport - perhaps you should be using one of the email-* flavours?');
+
+		return options.transport.sendMail({
+			to: options.to,
+			from: options.from,
+			subject: options.subject,
+			cc: options.cc,
+			bcc: options.bcc,
+			text: message.as('text'),
+			...(message.has('html') && {html: message.as('html')}),
+		});
+	}
+}
